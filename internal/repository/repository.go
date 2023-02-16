1364804b7983ca9cacfdf05b09b99306e3480648
Conrad Esche <conrad.3rd@gmail.com>
2023-02-16 10:20:37 +0100

    search availability by dates

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,10 +1,15 @@
 package repository
 
-import "github.com/conrad3rd/bookings/internal/models"
+import (
+	"time"
+
+	"github.com/conrad3rd/bookings/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUser() bool
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }
