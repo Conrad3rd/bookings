74e76043fcec8f36659db52beba1ccbcf5a9a360
Conrad Esche <conrad.3rd@gmail.com>
2023-02-27 18:35:43 +0100

    db seeds

---
+++ migrations/20230227172727_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('Generals Quarters','2023-02-11 00:00:00.000','2023-02-11 00:00:00.000'),
+	 ('Majors Suite','2023-02-13 00:00:00.000','2023-02-13 00:00:00.000');
