144c8778228a8a3c9a70cc6b8361905a58bda668
Conrad Esche <conrad.3rd@gmail.com>
2023-04-16 11:24:43 +0200

    add user migration

---
+++ migrations/20230416090719_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+	 ('Conrad','Esche','admin@admin.com','$2a$12$epZldW8n/hbvqyKqPKtH1.rtHlsyFPNpK5QcOWRui.Eke7bVhc8yW',3,'2023-03-15 00:00:00','2023-03-15 00:00:00');
